bcde20b9d2701d9973d72145ad7ba71b30dd97f6
leilazarov <stoneiiilei@gmail.com>
2021-10-22 01:19:30 +1100

    try react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import Home from './components/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const history = createBrowserHistory()
+
+const App = () => <>
+  <Router history={history}>
+    <Switch>
+      <Route
+        path='/'
+        exact
+        component={ () => <Home/>}
+      />
+    </Switch>
+  </Router>
+</>;
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+
+export default function Home() {
+  return (
+    <div>
+      <head>
+        <title>Create Next App</title>
+      </head>
+
+      <main >
+        <h1>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  )
+}
