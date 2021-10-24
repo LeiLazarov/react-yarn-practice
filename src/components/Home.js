61f7538a3b62ba0a311f6acfc9028e9264456df8
leilazarov <stoneiiilei@gmail.com>
2021-10-24 23:10:02 +1100

    add lib - styled-components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
 
 export default function Home () {
     const history = useHistory();
@@ -14,8 +15,12 @@ export default function Home () {
                 <h1>
                     Welcome to <a href='https://nextjs.org'>Next.js!</a>
                 </h1>
-                <button onClick={() => history.push('/about')}>Go to About us</button>
+                <Button onClick={() => history.push('/about')}>Go to About us</Button>
             </main>
         </div>
     );
 }
+
+const Button = styled.button`
+
+`;
